1c3a0209eb4a2c939c65c0ac50247851625638bb
Chauhan Dev <chauhandev2dc@gmail.com>
2024-08-11 15:58:46 +0530

    added /api on googleGeoRouter

--- routes/googleGeoRouter.js
+++ routes/googleGeoRouter.js
@@ -1,13 +1,14 @@
 const middlewares = require("../middleware/middleware.js");
 const googleGeoWebhook = require('../webhook/googleGeoWebhook.js')
+const { prepath } = require('../utilities');
 
 const googleGeoRouter = (app) => {
-    app.get("/place/autocomplete",
+    app.get(`${prepath}/place/autocomplete`,
         middlewares.isAuthenticate,
         googleGeoWebhook.getPlace
     );
 
-    app.get("/place/details",
+    app.get(`${prepath}/place/details`,
         middlewares.isAuthenticate,
         googleGeoWebhook.getPlaceDetails
     )
