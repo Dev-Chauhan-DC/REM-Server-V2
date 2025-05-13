2282845c0c9fd36bce9b002105636fd5739522ab
Chauhan Dev <chauhandev2dc@gmail.com>
2025-05-13 17:51:10 +0530

    timeout implemented

--- webhook/nominatim.js
+++ webhook/nominatim.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 
 const autocomplete = async (params) => {
     try {
-        const result = await axios.get(process.env.NOMINATIM_URL + '/search', { params });
+        const result = await axios.get(process.env.NOMINATIM_URL + '/search', { params, timeout: 5000 });
         return result.data;
     } catch (error) {
         throw error
--- webhook/photon.js
+++ webhook/photon.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 
 const autocomplete = async (params) => {
     try {
-        const result = await axios.get(process.env.PHOTON_URL + '/api', { params });
+        const result = await axios.get(process.env.PHOTON_URL + '/api', { params, timeout: 5000 });
         return result.data;
     } catch (error) {
         throw error
